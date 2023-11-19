ff1a9d84f06c2f728d73f1b72b177c780691efda
Juan Pablo Díaz Correa <juanipis@gmail.com>
2023-11-18 23:10:58 -0500

    Refactor report generation and add total hours

--- app/usecase/report/report_generator.py
+++ app/usecase/report/report_generator.py
@@ -2,12 +2,13 @@ import pandas as pd
 from app.routers.database import Database
 from datetime import datetime, time
 
+
 class ReportGenerator:
     def __init__(self):
         self.db = Database()
-        self.df = None  # Agregar una propiedad para el DataFrame
+        self.df = None
 
-    def generate_report(self, start_date, end_date, filename='report.xlsx'):
+    def generate_report(self, start_date, end_date, filename="report.xlsx"):
         start_datetime = datetime.combine(start_date, time())
         end_datetime = datetime.combine(end_date, time())
 
@@ -15,8 +16,26 @@ class ReportGenerator:
         data = []
         for task in tasks:
             if start_datetime <= task.start_time <= end_datetime:
-                duration = (task.end_time - task.start_time).total_seconds() / 3600  # Convertir a horas
-                data.append((task.id, task.start_time.strftime("%Y-%m-%d %H:%M:%S"), task.end_time.strftime("%Y-%m-%d %H:%M:%S"), task.description, duration))
-        
-        self.df = pd.DataFrame(data, columns=['ID', 'Start Time', 'End Time', 'Description', 'Duration'])
+                duration = (task.end_time - task.start_time).total_seconds() / 3600
+                data.append(
+                    (
+                        task.id,
+                        task.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                        task.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                        task.description,
+                        duration,
+                    )
+                )
+
+        self.df = pd.DataFrame(
+            data, columns=["ID", "Start Time", "End Time", "Description", "Duration"]
+        )
+
+        # Calcular y añadir la fila de total de horas
+        total_hours = self.df["Duration"].sum()
+        total_row = pd.DataFrame(
+            [["Total", "", "", "", total_hours]], columns=self.df.columns
+        )
+        self.df = pd.concat([self.df, total_row], ignore_index=True)
+
         self.df.to_excel(filename, index=False)
